0be900d25a608d6ffdba83ff9f52e7fabfdfc1f5
lazyeels <lazyeels@gmail.com>
2017-06-06 09:15:12 +0100

    first commit

---
+++ gitupdate.sh
@@ -0,0 +1,9 @@
+git config --global user.name "lazyeels"
+git config --global user.email "lazyeels@gmail.com"
+git add *.*
+git add js/*.*
+git add css/*.*
+git add img/*.*
+git add data/*.*
+git push -u origin master
+
--- js/game_class.js
+++ js/game_class.js
@@ -173,6 +173,23 @@ Game.prototype.Init = function(fps){
         e.preventDefault()
     }, false)
 
+    var canvas = this.canvas;
+    // Prevent scrolling when touching the canvas
+    document.body.addEventListener("touchstart", function (e) {
+      if (e.target == canvas) {
+        e.preventDefault();
+      }
+    }, false);
+    document.body.addEventListener("touchend", function (e) {
+      if (e.target == canvas) {
+        e.preventDefault();
+      }
+    }, false);
+    document.body.addEventListener("touchmove", function (e) {
+      if (e.target == canvas) {
+        e.preventDefault();
+      }
+    }, false);
     this.initialised = true;
 };
 
